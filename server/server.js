2ffcbb2e5d6b7673ada3d16d4ad9cc856e961264
nav-commits <53131408+nav-commits@users.noreply.github.com>
2024-03-24 13:43:31 -0400

    changed backend port

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
 
 const corsOptions = {
-    origin: 'https://mellow-brigadeiros-d0430a.netlify.app',
+    origin: 'https://fnlhockey.com',
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
